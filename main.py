519c4b8f045bf13bbdb78264e8c3046cad2f8d14
Netanel Friedman <netanel.friedman6@gmail.com>
2023-10-12 21:04:04 +0300

    Move everything to a class and add logging

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
 import os
-from typing import List
-from dotenv import load_dotenv
 import random
 
-from discord import Intents, Member, Client, Message
+from time import time
+from typing import List, Any
+from discord.utils import setup_logging
+from dotenv import load_dotenv
 
-INTENTS = Intents(members=True, messages=True, message_content=True)
-client: Client = Client(intents=INTENTS)
+import discord
+from discord import Intents, Member, Message
+from discord.abc import Messageable
 
-global all_curses
-all_curses: List[str]
+_log = setup_logging()
 
-@client.event
-async def on_ready() -> None:
-    print(f"{client.user} is ready")
+class CustomClient(discord.Client):
+    curses: List[str] = []
+    last_curse_time: float = 0
+    CURSE_DELTA_TIME = 3 * 60
 
+    def __init__(self, intents: Intents) -> None:
+        super().__init__(intents=intents)
 
-@client.event
-async def on_member_join(member: Member) -> None:
-    guild_channels = member.guild.text_channels
-    if len(guild_channels) == 0:
-        return
-    channel = guild_channels[0]
+        with open('curses.txt') as f:
+            self.curses = list(map(str.strip, f.readlines()))
 
-    k = random.randrange(1, 10)
-    curses = ', '.join(random.choices(all_curses, k=k))
-    await channel.send(f"{member.mention} {curses}.")
+        _log.info('successfully loaded curses')
 
+    async def send(self, to: Messageable, msg: str) -> Any:
+        _log.info(f"sending {msg} to {to}")
+        return await to.send(msg)
 
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return None
+    async def on_ready(self) -> None:
+        _log.info(f"{self.user} is ready")
+
+    async def on_member_join(self, member: Member) -> None:
+        guild_channels = member.guild.text_channels
+        if len(guild_channels) == 0:
+            return
+        channel = guild_channels[0]
+
+        k = random.randrange(1, 10)
+        curses = ', '.join(set(random.choices(self.curses, k=k)))
+        await self.send(channel, f"{member.mention} {curses}.")
+
+    async def on_message(self, message: Message) -> None:
+        if message.author == self.user:
+            return
+
+        now = time()
+        if now - self.last_curse_time < self.CURSE_DELTA_TIME:
+            return
+        self.last_curse_time = now
+
+        channel = message.channel
+        curse = random.choice(self.curses)
+        await self.send(channel, f"{message.author.mention} {curse}")
 
 
 def main():
@@ -40,12 +62,10 @@ def main():
     load_dotenv()
     TOKEN = os.getenv("TOKEN")
     if TOKEN is None:
-        print("Please provide TOKEN in a .env file")
+        _log.error("Please provide TOKEN in a .env file")
         exit(1)
+    INTENTS = Intents(members=True, messages=True, message_content=True)
+    CustomClient(INTENTS).run(TOKEN, log_handler=None)
 
-    with open('curses.txt') as f:
-        all_curses = list(map(str.strip, f.readlines()))
-
-    client.run(TOKEN)
 
 main()
